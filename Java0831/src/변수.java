4494cece11a2a9b4e24c3334e6785498e30ca3d7
김혜현 <91773938+hyehyeun@users.noreply.github.com>
2021-12-13 15:34:23 +0900

    1일차

---
+++ Java0831/src/변수.java
@@ -0,0 +1,17 @@
+
+public class 변수 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a = 3;
+        int weight = 50;
+        
+        System.out.println(a);
+        System.out.println("몸무계 : "+weight);
+        System.out.println("황해로 가고싶다");
+        System.out.println("해돋이 보러");
+        System.out.println("도망가자!!");
+	
+	}
+
+}
